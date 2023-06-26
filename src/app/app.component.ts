cee55d5baebca238b1ff780644ca4b03a252b025
VIplav Pandhurnekar <pandhurnekarviplav@gmail.com>
2023-06-26 10:07:22 +0530

    Header and slideshow complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'miehika-website';
+export class AppComponent implements OnInit {
+  title = 'Miehika Sahu | Visual Designer';
+  siteContent: any;
+  headerData: any;
+  homepage_slider: any;
+  jsonUrl = 'assets/content.json';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get(this.jsonUrl).subscribe((data) => {
+      this.siteContent = data;
+      this.headerData = this.siteContent.header;
+      this.homepage_slider = this.siteContent.homepage_slider;
+    });
+  }
 }
---
+++ src/app/common/slideshow/slideshow.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss'],
+})
+export class SlideshowComponent {
+  @Input() slideshow: any;
+}
